c64cdc3be56e0d1ea9d346e33970442c7189fc29
Prashant Singh <mprashantks@gmail.com>
2018-01-11 17:03:11 +0530

    Added problem 3

---
+++ problem-3/index.js
@@ -0,0 +1,7 @@
+function emailCredential() {
+  var subject = 'User credentials';
+  var body = 'Username: ' + $('#username').val() + '\n' + 'Password: ' +$('#password').val();
+  body = encodeURIComponent(body);
+
+  window.open('mailto:myemail@mydomain.com?subject='+subject+'&body='+body);
+}
