0fff05079f5976bf3ffa15945d369fd7149cbd9c
Prashant Singh <mprashantks@gmail.com>
2018-01-12 02:42:15 +0530

    Made some minor modifications and added comments

--- problem-4/index.js
+++ problem-4/index.js
@@ -1,6 +1,9 @@
+// List to store all selected options from box1 i.e., leftbox
 var li_items_leftbox = [];
+// List to store all selected options from box2 i.e., rightbox
 var li_items_rightbox = [];
 
+// Event listener to change colour of hovering list item
 $(document).on('mouseenter', 'li', function() {
   $(this).toggleClass('liHover');
 });
@@ -9,7 +12,7 @@ $(document).on('mouseleave', 'li', function() {
   $(this).toggleClass('liHover');
 });
 
-
+// Event listener to select list item from leftbox and insert that item into li_items_leftbox
 $(document).on('click', '#uItemLeft li', function() {
   $(this).toggleClass('liSelected');
 
@@ -24,7 +27,7 @@ $(document).on('click', '#uItemLeft li', function() {
   // console.log(li_items_rightbox);
 });
 
-
+// Event listener to select list item from rightbox and insert that item into li_items_rightbox
 $(document).on('click', '#uItemRight li', function() {
   $(this).toggleClass('liSelected');
 
@@ -39,8 +42,8 @@ $(document).on('click', '#uItemRight li', function() {
   // console.log(li_items_rightbox);
 });
 
-
-$('#move-right').click(function () {
+// Event listener to perform move operation when move-right button is clicked
+$(document).on('click', '#move-right', function () {
   li_items_leftbox.forEach(function (element) {
     $('#uItemRight').append('<li>'+element+'</li>');
     $('#uItemLeft li').filter(function () {return $(this).text() === element; }).remove();
@@ -48,8 +51,8 @@ $('#move-right').click(function () {
   li_items_leftbox = [];
 });
 
-
-$('#move-left').click(function () {
+// Event listener to perform move operation when move-left button is clicked
+$(document).on('click', '#move-left', function () {
   li_items_rightbox.forEach(function (element) {
     $('#uItemLeft').append('<li>'+element+'</li>');
     $('#uItemRight li').filter(function () {return $(this).text() === element; }).remove();
